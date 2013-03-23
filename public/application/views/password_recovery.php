e42fcb60c6ab2f9dfa01fc4009ef856bc5977f72
patricks <patrick@keydb.net>
2013-03-23 18:31:00 -0500

    Deleted node

--- public/application/views/password_recovery.php
+++ public/application/views/password_recovery.php
@@ -1,19 +1,25 @@
+<div class= "img-glass">
+<div id= "header-marquee">
 <div class= "wrapper">
-<h3>Retrieve Your Account</h3>
-	<div class= "pw-recovery">
-		<?php
-			if(!isset($pwRecoverUsername)) {
-				echo form_open('password_recovery/identify');
-				echo '<p>Enter your email or username associated with the account.</p>';
-				echo form_input('identification', '', 'placeholder= "Email or Username"');
-				echo anchor('#', 'I don&#39;t remember my email or username', 'id= "forgot-password-link"');
-			}
-			else if(isset($pwRecoverUsername)) {
-				echo '<p>Email: ' . $pwRecoverEmail . '</p>';
-				echo '<p>Username: ' . $pwRecoverEmail . '</p>';
-				echo '<p>An email to reset your password has been sent to the email provided on your account.</p>';
-			}
-			
-		?>
+	<div class= "header-logged-out">
 	</div>
+	<div id= "pw-recovery">
+		<h3>Retrieve Your Account</h3>
+		<div class= "divider"></div>
+			<?php
+				if(!isset($pwRecoverUsername)) {
+					echo form_open('password_recovery/identify');
+					echo 'Enter your email or username associated with the account.';
+					echo '<p class= "pw-recovery-align">' . form_input('identification', '', 'placeholder= "Email or Username"');
+					echo anchor('#', 'I don&#39;t remember my email or username', 'id= "forgot-password-link"') . '</p>';
+				}
+				else if(isset($pwRecoverUsername)) {
+					echo '<p>Email: ' . $pwRecoverEmail . '</p>';
+					echo '<p>Username: ' . $pwRecoverEmail . '</p>';
+					echo 'An email to reset your password has been sent to the email provided on your account.';
+				}	
+			?>
+	</div>
+</div>
+</div>
 </div>
